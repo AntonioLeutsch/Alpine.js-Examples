685897a88c28c689b17db3c9d3261e808e2e9344
Antonio Leutsch <post@antonio-leutsch.com>
2021-10-25 12:06:38 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./index.html", "./fetch/**/*.html", "./tabs/**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
